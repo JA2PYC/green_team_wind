e08ebc81fba3ca9fbaa11d6547ea8cc0fcc692d0
ja2pyc <ja2pyc@gmail.com>
2024-12-19 16:22:12 +0900

    Set Blueprint

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, jsonify
+from flask import Flask, Blueprint, render_template, request, jsonify
 from api.kma_sfctm2 import fetch_kma_sfctm2_data
 # from api.open_api_PvAmountByPwrGen import fetch_power_data
 # from api.open_api_wind_power_by_hour import fetch_wind_data
@@ -6,7 +6,9 @@ from models.random_forest_model import rf_model_predict
 import random
 import json
 
-app = Flask(__name__)
+# 라우트 설정
+# app = Flask(__name__)
+dashboard_bp = Blueprint('dashboard', __name__)
 
 # from flask_mysqldb import MySQL
 # app.config['MYSQL_HOST'] = 'localhost' 
@@ -16,12 +18,12 @@ app = Flask(__name__)
 # mysql = MySQL(app)
 
 # 라우트 설정
-@app.route('/')
+@dashboard_bp.route('/')
 def dashboard():
     return render_template('dashboard.html')
 
 # # 공공 데이터 PvAmountByPowerGen API
-# @app.route("/api/power", methods=["POST"])
+# @dashboard_bp.route("/api/power", methods=["POST"])
 # def power_data():
 #     # 클라이언트로부터 요청받은 파라미터
 #     params = request.json
@@ -36,7 +38,7 @@ def dashboard():
 #     return jsonify(result)
 
 # 기상 데이터 
-@app.route("/api/kma_sfctm2", methods=["POST"])
+@dashboard_bp.route("/api/kma_sfctm2", methods=["POST"])
 def kma_sfctm2_data():
     try:
         # 클라이언트로부터 요청받은 파라미터
@@ -51,7 +53,7 @@ def kma_sfctm2_data():
         return jsonify({'error' : str(e)}), 500
 
 # RF 모델
-@app.route("/model/rf_model", methods=["POST"])
+@dashboard_bp.route("/model/rf_model", methods=["POST"])
 def rf_model_data():
     try:
         # # Temperature (°C), "wind velocity (m/s)," "local atmospheric pressure (hPa)," "air density (kg/m^3)"
@@ -71,7 +73,7 @@ def rf_model_data():
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
-@app.route('/api/chart_data', methods=['POST'])
+@dashboard_bp.route('/api/chart_data', methods=['POST'])
 def chart_data():
     # 클라이언트에서 보낸 데이터를 가져옴
     received_data = request.json
@@ -87,7 +89,7 @@ def chart_data():
     return jsonify(data)
 
 # Dummy Chart
-# @app.route('/data')
+# @dashboard_bp.route('/data')
 # def get_data():
 #     # 더미 데이터 생성 (실제 데이터로 교체 가능)
 #     data = {
