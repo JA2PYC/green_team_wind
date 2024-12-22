d73335c160f7ff7df69e691ba1fd31c239cf7178
G2N <gr2nade@gmail.com>
2024-12-23 00:06:12 +0900

    Script Path Problem

--- models/jeju_cstl_ai_model.py
+++ models/jeju_cstl_ai_model.py
@@ -1,10 +1,11 @@
 import os
+import sys
 import subprocess
 import pandas as pd
 from data.cstl_ai_data import create_model_pkl
 import joblib
 
-createname = 'cstl_ai_data'
+scriptname = 'cstl_ai_data'
 dataname = 'Jeju_MergedData_20241217'
 modelname = 'jeju_cstl_ai'
 file_dir = os.path.dirname(__file__)
@@ -12,17 +13,21 @@ pkl_path = os.path.join(file_dir, 'saved_models', f'{modelname}.pkl')
 scaler_X_path = os.path.join(file_dir, 'saved_models', f'{modelname}_scaler_X.pkl')
 scaler_y_path = os.path.join(file_dir, 'saved_models', f'{modelname}_scaler_y.pkl')
 
+script_path = os.path.join(file_dir, 'data', f'{scriptname}.py')
+
 if not os.path.exists(pkl_path):
     print (f'{modelname}.pkl 파일이 존재하지 않습니다. 새로 생성합니다...')
     try:
-        subprocess.run([sys.executable, create_model_pkl, dataname, modelname], check=True)
+        # create_model_pkl(dataname, modelname)
+        # subprocess.run(['python', create_model_pkl, dataname, modelname], check=True)
+        subprocess.run([sys.executable, script_path, dataname, modelname], check=True)
     except Exception as e:
         raise RuntimeError(f'{modelname}.pkl 파일 생성중 오류 발생: {e}')
     
 try:
-    model_file = joblib.load(open(pkl_path), 'rb')
-    scaler_X = joblib.load(open(scaler_X_path), 'rb')
-    scaler_y = joblib.load(open(scaler_y_path), 'rb')
+    model_file = joblib.load(open(pkl_path, 'rb'))
+    scaler_X = joblib.load(open(scaler_X_path, 'rb'))
+    scaler_y = joblib.load(open(scaler_y_path, 'rb'))
 except Exception as e:
     raise RuntimeError(f'모델 로드 중 오류 발생: {e}')
 
